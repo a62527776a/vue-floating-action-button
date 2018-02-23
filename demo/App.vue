7defa0f3a1c9b24a4c09f994930283753e52daad
xiaoyao_01 <377687554@qq.com>
2018-02-23 18:48:11 +0800

    修饰demo页

--- demo/App.vue
+++ demo/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main">
     <div class="btn btn-lg btn-block btn-sm btn-info" @click="isEdit = !isEdit">配置项</div>
+    <transition name="fade">
     <div v-if="isEdit">
     <div class="form-group">
       <h6 for="mainIcon">主ICON (本组件所有ICON皆可从https://material.io/icons/中查找)</h6>
@@ -20,25 +21,32 @@
     </div>
     <div class="btn btn-lg btn-block btn-sm btn-info" @click="hidden = !hidden">是否隐藏: {{hidden}}</div>
     <br />
-    <form v-for="(item, idx) in menu" class="form-horizontal">
-      <label for="inputEmail3" class="btn-block">子菜单{{idx}}
-        <button type="button" @click="menu.splice(idx, 1)" class="btn btn-danger btn-xs pull-right">DELETE</button>
-      </label>
-      <div class="form-group" v-for="(value, key) in item">
-        <h6 for="inputEmail3" class="col-xs-2 col-lg-1 text-left">{{key}}</h6>
-        <div class="col-xs-10 col-lg-11">
-          <input v-model="item[key]" class="form-control" id="inputEmail3" :placeholder="key">
+    <transition-group name="list-complete" tag="div">
+      <form v-for="(item, idx) in menu" :key="idx" class="form-horizontal">
+        <b style="margin-bottom: 15px" class="text-primary btn-block">子菜单{{idx}}
+          <button type="button" @click="menu.splice(idx, 1)" class="btn btn-danger btn-xs pull-right">DELETE</button>
+        </b>
+        <div class="form-group" v-for="(value, key) in item">
+          <h6 class="col-xs-2 col-lg-1 text-left">{{key}}</h6>
+          <div class="col-xs-10 col-lg-11">
+            <input v-model="item[key]" class="form-control" :placeholder="key">
+          </div>
         </div>
+      </form>
+    </transition-group>
+    <transition name="fade">
+      <div v-show="isAddChildMenuBtn" class="btn btn-lg btn-block btn-sm btn-info" 
+          @click="menu.push({
+            key: '',
+            icon: '',
+            title: '',
+            color: ''
+          })">新增子菜单
       </div>
-    </form>
-    <div class="btn btn-lg btn-block btn-sm btn-info" 
-         @click="menu.push({
-           key: '',
-           icon: '',
-           title: '',
-           color: ''
-         })">新增子菜单</div> <br />
+    </transition>
+    <br />
     </div>
+    </transition>
     <vue-fab 
       @clickItem="clickItem"
       @clickMainBtn="clickMainBtn"
@@ -63,6 +71,8 @@
         spacing: 45,
         fabAnimateBezier: 'ease',
         isEdit: false,
+        isAddChildMenuBtn: true,
+        addChildMenuTiming: null,
         menu: [
           {
             key: 'add',
@@ -77,6 +87,17 @@
             color: '#9B5BB6'
           }
         ]
+      }
+    },
+    watch: {
+      "menu.length": function (val) {
+        if (this.addChildMenuTiming) return
+        this.isAddChildMenuBtn = false
+        this.addChildMenuTiming = setTimeout(() => {
+          this.isAddChildMenuBtn = true
+          clearTimeout(this.addChildMenuTiming)
+          this.addChildMenuTiming = null
+        }, 1000)
       }
     },
     methods: {
@@ -96,5 +117,31 @@
 <style lang="less" scoped>
 .main {
   padding: 15px;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.list-complete-item {
+  transition: all 1s;
+  display: block;
+  margin-right: 10px;
+}
+.list-complete-enter {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.list-complete-leave-to {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.form-horizontal {
+  transition: all 1s;
+  display: block;
+  margin-right: 10px;
 }
 </style>
