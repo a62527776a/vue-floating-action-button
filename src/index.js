525737f3e4665412dfb5686b903893b773e6daf7
xiaoyao_01 <377687554@qq.com>
2018-02-26 15:10:48 +0800

    兼容ipad

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import FAB from './fab.vue'
 import FABCantainer from './fab-cantainer.vue'
 
+/**
+ * @method testSafariBrower 处理safari浏览器body无法响应click事件
+ * @return { Boolean } true / false
+ */
+let testSafariBrower = () => {
+  if (navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/Mac/i)) {
+    return true
+  } else {
+    return false
+  }
+}
+
 const install = (Vue) => {
   Vue.component(FAB.name, FAB)
   Vue.component(FABCantainer.name, FABCantainer)
@@ -14,9 +26,7 @@ const install = (Vue) => {
       }
       el.__clickOutside__ = listenClick
       // 处理safari浏览器body对象无法响应click事件
-      if (navigator.userAgent.indexOf("iPhone") > -1) {
-        document.querySelector('body').setAttribute('class', 'setCursor')
-      }
+      if (testSafariBrower()) document.querySelector('body').setAttribute('class', 'setCursor')
       document.body.addEventListener('click', listenClick)
     },
     unbind: (el, binding) => {
