b86ddcdec4509b074a15aa2b3958bf803d5ab2db
xiaoyao_01 <377687554@qq.com>
2018-02-26 11:30:55 +0800

    test

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ const install = (Vue) => {
         binding.value()
       }
       el.__clickOutside__ = listenClick
+      // 处理safari浏览器body对象无法响应click事件
+      if (navigator.userAgent.indexOf("Safari") > -1) {
+        document.body.style = {
+          background: '#F4F4F4',
+          cursor: 'pointer',
+          '-webkit-tap-highlight-color': 'transparent'
+        }
+      }
       document.body.addEventListener('click', listenClick)
     },
     unbind: (el, binding) => {
