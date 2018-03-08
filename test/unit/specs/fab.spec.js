f3c8a55a0c8fd91af4f698385ba90fc8e49dda57
a62527776a <a13868300128a@126.com>
2018-03-08 14:37:11 +0800

    单元测试模块

--- test/unit/specs/fab.spec.js
+++ test/unit/specs/fab.spec.js
@@ -98,33 +98,63 @@ describe('fab.vue', () => {
     expect(wrapper.vm.active).toBe(false)
   })
 
-  // it('测试滚动自动隐藏事件', () => {
-  //   document.body.style.height = 3000 + 'px'
-  //   for (let i = 0; i < 50; i++) {
-  //     window.scrollBy(0, 5)
-  //     // console.log(document.documentElement.scrollTop)
-  //     // console.log(wrapper.vm.scrollDirection)
-  //     console.log(wrapper.vm.scrollTop)
-  //     console.log(document.documentElement.scrollTop)
-  //   }
-  //   for (let i = 0; i < 50; i++) {
-  //     window.scrollBy(0, -5)
-  //     // console.log(document.documentElement.scrollTop)
-  //     // console.log(wrapper.vm.scrollDirection)
-  //     console.log(wrapper.vm.scrollTop)
-  //     console.log(document.documentElement.scrollTop)
-  //   }
-  //   expect(wrapper.vm.hidden).toBe(false)
-  // })
+  it('测试滚动自动隐藏事件', () => {
+    // document.body.style.height = 3000 + 'px'
+    // for (let i = 0; i < 50; i++) {
+    //   window.scrollBy(0, 5)
+    // }
+    // for (let i = 0; i < 50; i++) {
+    //   window.scrollBy(0, -5)
+    // }
+    // setTimeout( _ => {
+    //   expect(wrapper.vm.hidden).toBe(false)
+    //   done()
+    // }, 600)
+  })
 
   it('测试子菜单点击后自动关闭', (done) => {
     wrapper.find('.fab').trigger('click')
-    wrapper.find('.fab-item .material-icons').trigger('click')
-    wrapper.vm.$nextTick(() => {
-      // 300毫秒后active为false
-      // expect(wrapper.vm.active).toBe(false)
+    wrapper.find('.fab-item').trigger('click')
+    setTimeout( _ => {
+      expect(wrapper.vm.active).toBe(false)
       done()
+    }, 600)
+    // 测试props clickAutoClose 属性是否生效
+    // false 预期为点击后不关闭子菜单
+    wrapper.setProps({
+      clickAutoClose: false
     })
+    expect(wrapper.vm.clickAutoClose).toBe(false)
+    // 打开子菜单
+    wrapper.find('.fab').trigger('click')
+    // 点击子菜单
+    wrapper.find('.fab-item').trigger('click')
+    // 预期不自动关闭子菜单
+    setTimeout( _ => {
+      expect(wrapper.vm.active).toBe(true)
+      done()
+    }, 600)
+  })
+  
+  it('测试子菜单props idx属性对top、transitionDelay属性的影响', (done) => {
+    // 给每个子菜单复制idx属性
+    for (let i = 1; i < 4; i++) {
+      wrapper.vm.$children[i].idx = i - 1
+    }
+    // 打开子菜单
+    wrapper.find('.fab').trigger('click')
+    let wrappers = wrapper.findAll('.fab-item')
+    setTimeout(_ => {
+      for (let i = 0; i < wrappers.length; i++) {
+        // -40 - this.idx * this.$parent.globalOptions.spacing + 'px'
+        let topReg = new RegExp(`${-40 - i * wrapper.vm.globalOptions.spacing}px`)
+        // this.idx * this.$parent.globalOptions.delay + 's'
+        let delayReg = new RegExp(`${i * wrapper.vm.globalOptions.delay}s`)
+        expect(wrappers.wrappers[i].attributes().style).toMatch(topReg)
+        expect(wrappers.wrappers[i].attributes().style).toMatch(delayReg)
+      }
+      done()
+    }, 600)
   })
 
 })
