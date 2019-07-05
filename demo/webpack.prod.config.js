87d89db8c1ae860a23696e73d945307cd041b87a
陈雨 <--name-only>
2019-07-05 19:25:14 +0800

    更新官网

--- demo/webpack.prod.config.js
+++ demo/webpack.prod.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, '/main.js')
+    main: path.resolve(__dirname + '/main.js')
   },
   output: {
     path: path.resolve(__dirname, '../docs/'),
