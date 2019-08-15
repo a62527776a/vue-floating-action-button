d1e03716c1efd8f19c23da6fe084328aee36b9bd
a62527776a <a13868300128a@126.com>
2019-08-15 23:18:40 +0800

    11

--- bin/publish.js
+++ bin/publish.js
@@ -1,6 +1,18 @@
 var exec = require('child_process').execSync
 
-let str = exec(`scp -r ./docs/ root@${process.env.host}:/data/wwwroot/vue-floation-action-button`)
+var str = exec(`scp -r ./docs/ root@${process.env.host}:/data/wwwroot/vue-floation-action-button`)
+
+console.log(str.toString("utf8").trim());
+
+console.log('上传成功')
+
+var str = exec(`scp ./docs/index.html root@${process.env.host}:/data/wwwroot/`)
+
+console.log(str.toString("utf8").trim());
+
+console.log('上传成功')
+
+var str = exec(`scp ./docs/vue-fab.js root@${process.env.host}:/data/wwwroot/`)
 
 console.log(str.toString("utf8").trim());
 
