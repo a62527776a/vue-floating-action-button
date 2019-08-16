a4c6fa719a87c933bd91c42f0257ede2c52ffd69
陈雨 <--name-only>
2019-08-16 11:56:46 +0800

    update publish.js

--- bin/publish.js
+++ bin/publish.js
@@ -5,3 +5,9 @@ var str = exec(`scp -r ./docs/ root@${process.env.host}:/data/wwwroot/vue-floati
 console.log(str.toString("utf8").trim());
 
 console.log('上传成功')
+
+var str = exec(`scp -r ./docs/homepage/homepage root@${process.env.host}:/data/wwwroot/vue-floating-action-button/`)
+
+console.log(str.toString("utf8").trim());
+
+console.log('上传成功')
